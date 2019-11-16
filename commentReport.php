198374cc8e55aa1dc764c7f5d19a84cb0fa856d3
Joe Strahm <jstrahm@verabradley.com>
2019-11-16 09:00:17 -0500

    initial files

---
+++ commentReport.php
@@ -0,0 +1,18 @@
+<?php
+  include_once $_SERVER['DOCUMENT_ROOT'] . "/report/CommentReport.php"
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Web Programmer Test Project - Comment Report</title>
+</head>
+<body>
+<h1>Comments are grouped by the following sections</h1>
+<h2>Comments about candy</h2>
+<h2>Comments about call me / don't call me</h2>
+<h2>Comments about who referred me</h2>
+<h2>Comments about signature requirements upon delivery</h2>
+<h2>Miscellaneous comments (everything else)</h2>
+</body>
+</html>
