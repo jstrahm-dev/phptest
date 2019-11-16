2fdefe892dabc9d07a540445e320d4ce6a8ddda1
Joe Strahm <jstrahm@verabradley.com>
2019-11-16 13:29:40 -0500

    Task #2 fixing shipping date

---
+++ fixShipDate.php
@@ -0,0 +1,14 @@
+<?php
+  include_once $_SERVER['DOCUMENT_ROOT'] . "/utility/FixShipDate.php";
+  FixShipDate::proc();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Web Programmer Test Project - Fix Ship Date</title>
+</head>
+<body>
+<h1>Done</h1>
+</body>
+</html>
---
+++ utility/FixShipDate.php
@@ -0,0 +1,70 @@
+<?php
+
+  class FixShipDate {
+
+    function proc() {
+      $fixShipDate = new FixShipDate();
+      $records = $fixShipDate->getCommentsWithExpectedShipDates();
+      for ($i=0; $i<count($records); $i++) {
+        $fixShipDate->fix($records[$i]);
+      }
+    }
+
+    function fix($record) {
+      $orderId = $record["orderid"];
+      $comment = trim(preg_replace('/\s\s+/', ' ', trim($record["comment"])));
+      $dateString = substr($comment, -8);
+      $time = strtotime($dateString);
+      $newformat = date('Y-m-d',$time);
+      $dbconn = mysqli_connect("localhost","devuser01","Ur2good8!","dev01");
+      if (!$dbconn) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+      }
+
+      $sql = "update sweetwater_test set shipdate_expected='" . $newformat . "'";
+      $sql .= " WHERE orderid='" . $orderId . "' limit 1";
+
+      echo "Record OrderId: " . $orderId . " ship date: " . $newformat;
+      if ($dbconn->query($sql) === TRUE) {
+        echo " Record updated successfully<br />";
+      } else {
+        echo " Error updating record: " . $conn->error . "<br />";
+      }
+
+      $dbconn->close();
+
+    }
+
+    function getCommentsWithExpectedShipDates() {
+      $fixRecords = array();
+      $dbconn = mysqli_connect("localhost","devuser01","Ur2good8!","dev01");
+      if (!$dbconn) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+      }
+
+      $sql = "SELECT orderid, comments FROM sweetwater_test";
+      $sql .= " WHERE comments like '%Expected Ship Date%'";
+      $result = $dbconn->query($sql);
+
+      if ($result->num_rows > 0) {
+        $x = 0;
+        while($row = $result->fetch_assoc()) {
+          $fixRecords[$x] = array (
+            orderid  => $row["orderid"],
+            comment  => $row["comments"]
+          );
+          $x++;
+        }
+      }
+
+      $dbconn->close();
+      return $fixRecords;
+    }
+  }
+?>
